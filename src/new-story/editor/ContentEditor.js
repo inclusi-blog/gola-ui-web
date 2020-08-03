df9f3587b51632e641864ce87c8c185a91f475b2
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-08-03 23:13:30 +0530

    [Harish] : Add line height and hovering toolbar for content editor

--- src/new-story/editor/ContentEditor.js
+++ src/new-story/editor/ContentEditor.js
@@ -2,6 +2,9 @@ import React, { useState, useMemo } from 'react';
 import { createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 import { withHistory } from 'slate-history';
+import HoveringToolbar from "./HoveringToolbar";
+import Leaf from "./Leaf";
+import { toggleFormat } from "./utils/formatUtils";
 
 const initialValue = [
   {
@@ -16,7 +19,26 @@ const ContentEditor = () => {
   const editor = useMemo(() => withHistory(withReact(createEditor())), []);
   return (
     <Slate editor={editor} value={value} onChange={changedText => setValue(changedText)}>
+      <HoveringToolbar />
       <Editable
+        style={{
+          fontSize: 24,
+          lineHeight: '177%'
+        }}
+        renderLeaf={props => <Leaf {...props} />}
+        /* eslint-disable-next-line consistent-return */
+        onDOMBeforeInput={event => {
+          switch (event.inputType) {
+            case 'formatBold':
+              return toggleFormat(editor, 'bold');
+            case 'formatItalic':
+              return toggleFormat(editor, 'italic');
+            case 'formatUnderline':
+              return toggleFormat(editor, 'underline');
+            default:
+              break;
+          }
+        }}
         placeholder="Enter some plain text..." />
     </Slate>
   );
