13866806b77d54fa5aa3cf9af760d075b934c08a
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-09-24 11:08:42 +0530

    [Harish] : Wire up username availability api to chcek username available on registration

--- src/welcome/signup/UsernameInputScreen.js
+++ src/welcome/signup/UsernameInputScreen.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import { debounce } from 'lodash';
+import React, { useRef, useState } from 'react';
+import ajax from '../../helpers/ajaxHelper';
 import {
   ContinueButton,
   SignInText,
@@ -9,11 +11,35 @@ import {
 } from './Signup.style';
 
 const UsernameInputScreen = () => {
-  // eslint-disable-next-line no-unused-vars
   const [usernameLabel, setUserNameLabel] = useState('Username');
   const [username, setUsername] = useState('');
-  // eslint-disable-next-line no-unused-vars
   const [usernameExistsErr, setUsernameExistsErr] = useState(false);
+
+  const checkUsernameAvailability = (value) => {
+    if (value.length === 0) {
+      setUserNameLabel('Please enter a username');
+      setUsernameExistsErr(true);
+      return;
+    }
+    ajax
+      .post('/idp/v1/user/usernameAvailability', { username: value })
+      .then(({ data }) => {
+        const { isUsernameAvailable } = data;
+        if (isUsernameAvailable) {
+          setUsernameExistsErr(true);
+          setUserNameLabel('Username already exist');
+          return;
+        }
+        setUserNameLabel('Username');
+        setUsernameExistsErr(false);
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      });
+  };
+
+  const delayedHandleChange = useRef(debounce((value) => checkUsernameAvailability(value), 700)).current;
 
   return (
     <div
@@ -28,11 +54,16 @@ const UsernameInputScreen = () => {
     >
       <UsernameUniqueTitle>Let’s keep you unique! choose a username.</UsernameUniqueTitle>
       <UsernameInputWrapper>
-        <UsernameLabel>{usernameLabel}</UsernameLabel>
+        <UsernameLabel isError={usernameExistsErr}>{usernameLabel}</UsernameLabel>
         <UsernameInput
           isError={usernameExistsErr}
           value={username}
-          onChange={({ target }) => setUsername(target.value)}
+          onChange={({ target }) => {
+            setUsername(target.value);
+            setUserNameLabel('Username');
+            setUsernameExistsErr(false);
+            delayedHandleChange(target.value);
+          }}
         />
       </UsernameInputWrapper>
       <div style={{ marginTop: 92 }}>
