5f793720a5d14e89826014b291e601e4845de466
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-10-31 11:48:43 +0530

    [Harish] : Add stories title and import new story buttons

--- src/Screens/stories/Stories.js
+++ src/Screens/stories/Stories.js
@@ -4,10 +4,22 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Drafts from './Drafts';
 import Published from './Published';
 import { RoutedTabs, NavTab } from './router-nav-tabs';
+import { ButtonLabel, NewStoryButton, StoriesTitle, StoriesTitleRow } from './Stories.style';
 
 export default ({ match }) => {
   return (
-    <div style={{ marginTop: 190 }}>
+    <div style={{ width: 760 }}>
+      <StoriesTitleRow>
+        <StoriesTitle>Stories</StoriesTitle>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <NewStoryButton isBackgroundLessButton style={{ marginRight: 30 }}>
+            <ButtonLabel isBackgroundLessButton>IMPORT</ButtonLabel>
+          </NewStoryButton>
+          <NewStoryButton isBackgroundLessButton={false}>
+            <ButtonLabel isBackgroundLessButton={false}>New story</ButtonLabel>
+          </NewStoryButton>
+        </div>
+      </StoriesTitleRow>
       <RoutedTabs startPathWith={match.path} style={{ borderBottom: '1px solid #9A9A9A', width: 760 }}>
         <NavTab to="/drafts">Drafts</NavTab>
         <NavTab to="/published">Published</NavTab>
---
+++ src/Screens/stories/Stories.style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { Button, NewStoryLabel } from '../user/UserPublication.style';
+
+export const StoriesTitle = styled.p`
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 72px;
+  color: #2c363f;
+`;
+
+export const StoriesTitleRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NewStoryButton = styled(Button)`
+  height: 20px;
+  padding: 8px 20px;
+  background-color: ${(props) => (props.isBackgroundLessButton ? 'white' : '#FA163F')};
+  cursor: default;
+`;
+
+export const ButtonLabel = styled(NewStoryLabel)`
+  color: ${(props) => (props.isBackgroundLessButton ? '#FA163F' : 'white')};
+`;
