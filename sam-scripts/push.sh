b98a22b24f1dcf6264680a5895110acf64d67fd5
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-11-07 12:52:43 +0530

    [Harish|Monesh|Thamizh] : Change commit revision enironment variable name in push tartget

--- sam-scripts/push.sh
+++ sam-scripts/push.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 source $SOURCE_DIR/docker-artifactory-login.sh
 
 echo "Pushing docker images to Artifactory"
 
 docker login -u $ARTIFACTORY_USER -p $ARTIFACTORY_PASSWORD
-docker tag gola-ui-web gola05/gola-ui-web:$GO_REVISION_GOLA_UI_WEB
-docker push $ARTIFACTORY_USER/gola-ui-web:$GO_REVISION_GOLA_UI_WEB
+docker tag gola-ui-web gola05/gola-ui-web:$JENKINS_REVISION_GOLA_UI_WEB
+docker push $ARTIFACTORY_USER/gola-ui-web:$JENKINS_REVISION_GOLA_UI_WEB
 
 echo "cleaning pushed docker image  ... "
-docker rmi $ARTIFACTORY_USER/gola-ui-web:$GO_REVISION_GOLA_UI_WEB
+docker rmi $ARTIFACTORY_USER/gola-ui-web:$JENKINS_REVISION_GOLA_UI_WEB
 
 if [ $retVal -ne 0 ]; then
     echo "Push of  docker image failed"
