4c163fe623fb52a29a5f221cb63e50146ecbe022
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-11-20 00:06:57 +0530

    [Harish|Vignesh] : Align post share pallate center

--- src/common-components/PostSharePalate.js
+++ src/common-components/PostSharePalate.js
@@ -3,28 +3,21 @@ import TwitterImg from 'assets/images/Twitter.png';
 import FacebookImg from 'assets/images/Facebook.svg';
 import WhatsAppImg from 'assets/images/whatsApp.svg';
 import LinkedInImg from 'assets/images/LinkedIn.svg';
-import {
-    TwitterIcon,
-    FacebookIcon,
-    WhatsAppIcon,
-    LinkedInIcon,
-    SmallDots,
-    ApplyRow
-}from './PostSharePalate.style';
+import { TwitterIcon, FacebookIcon, WhatsAppIcon, LinkedInIcon, SmallDots, ApplyRow } from './PostSharePalate.style';
 
-const PostSharePalate = () =>{
-    return(
-        <ApplyRow>
-            <TwitterIcon src ={TwitterImg}/>
-            <FacebookIcon src={FacebookImg}/>
-            <WhatsAppIcon src={WhatsAppImg}/>
-            <LinkedInIcon src={LinkedInImg}/>
-            <ApplyRow>
-                <SmallDots/>
-                <SmallDots/>
-                <SmallDots/>
-            </ApplyRow>      
-        </ApplyRow>
-    )
-}
+const PostSharePalate = () => {
+  return (
+    <ApplyRow style={{ alignItems: 'center' }}>
+      <TwitterIcon src={TwitterImg} />
+      <FacebookIcon src={FacebookImg} />
+      <WhatsAppIcon src={WhatsAppImg} />
+      <LinkedInIcon src={LinkedInImg} />
+      <ApplyRow>
+        <SmallDots />
+        <SmallDots />
+        <SmallDots />
+      </ApplyRow>
+    </ApplyRow>
+  );
+};
 export default PostSharePalate;
