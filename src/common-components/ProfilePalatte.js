79f9b9ee46f65fc48b0aae2389067d71fb5746d6
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-11-20 00:11:24 +0530

    [Harish|Vignesh] : Fix Alignment for profile palatte

---
+++ src/common-components/ProfilePalatte.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import AuthorProfile from 'assets/images/profile.png';
+import PostSharePalate from 'common-components/PostSharePalate';
+import {
+  AuthorProfilePic,
+  AuthorName,
+  PostedDate,
+  PostReadTime,
+  FollowButton,
+  FollowLabel,
+  ApplyRow,
+  ApplyColumn
+} from './ProfilePalatte.style';
+
+const ProfilePalatte = () => {
+
+  return (
+    <ApplyRow style={{ justifyContent: 'space-between'}}>
+      <div style={{display: 'flex'}}>
+        <ApplyRow style={{ alignItems: 'center'}}>
+          <AuthorProfilePic src={AuthorProfile}/>
+          <ApplyColumn style={{ marginLeft: 19, marginRight: 34}}>
+            <AuthorName>Swetha suresh</AuthorName>
+            <ApplyRow style={{ marginTop: 8, width: 133, justifyContent: 'space-between'}}>
+              <PostedDate>8 July</PostedDate>
+              <PostReadTime>5 min read</PostReadTime>
+            </ApplyRow>
+          </ApplyColumn>
+          <FollowButton>
+            <FollowLabel>Follow</FollowLabel>
+          </FollowButton>
+        </ApplyRow>
+      </div>
+
+      <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <PostSharePalate/>
+      </div>
+    </ApplyRow>
+  );
+};
+
+export default ProfilePalatte;
--- src/common-components/ProfilePalatte.style.js
+++ src/common-components/ProfilePalatte.style.js
@@ -1,28 +1,11 @@
 import styled from 'styled-components';
 
-export const ProfilePicRightLine = styled.div`
-    width: 53.74px;
-    height: 11.13px;
-    border: 1px solid #FA163F;
-    box-sizing: border-box;
-    transform: rotate(90deg);
-`;
-
-export const ProfilePicLeftLine = styled.div`
-    width: 53.74px;
-    height: 11.13px;
-    border: 1px solid #FA163F;
-    box-sizing: border-box;
-    transform: rotate(90deg);  
-    `;
-
 export const AuthorProfilePic = styled.img`
     width: 67px;
     height: 67px;
 `;
 
 export const AuthorName = styled.p`
-    width: 133px;
     height: 23px;
     font-family: Poppins;
     font-style: normal;
@@ -30,6 +13,7 @@ export const AuthorName = styled.p`
     font-size: 18px;
     line-height: 129%;
     color: #3B3B58;
+    margin: 0;
 `;
 
 export const PostedDate = styled.p`
@@ -47,6 +31,7 @@ export const PostReadTime = styled.p`
     font-style: normal;
     font-weight: 300;
     font-size: 18px;
+    margin: 0;
     line-height: 129%;
     color: #535353;
 `;
@@ -56,14 +41,17 @@ export const FollowButton = styled.div`
     height: 31px;
     background: #FA163F;
     border-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const FollowLabel = styled.p`
     font-family: Poppins;
     font-style: normal;
-    font-weight: normal;
+    font-weight: 400;
     font-size: 18px;
-    line-height: 129%;
+    line-height: 23.22px;
     color: #FFFFFF;
     margin: 0;
 `;
