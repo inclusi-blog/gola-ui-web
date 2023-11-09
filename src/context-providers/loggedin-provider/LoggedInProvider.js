b60dc9adde4891f96a1dd4b3072c6bf3e92c311b
Hariharan Sivakumar <hariharansivakumar@affordmed.com>
2023-11-09 03:48:21 -0500

    Comment save

--- src/context-providers/loggedin-provider/LoggedInProvider.js
+++ src/context-providers/loggedin-provider/LoggedInProvider.js
@@ -26,22 +26,22 @@ const LoggedInProvider = ({ children }) => {
     setIsLoginDone(true);
   }, []);
 
-  const saveToken = (tokenData) => {
-    let isSecure = true;
-    if (CONFIGS.ENVIRONMENT.toLowerCase() === 'local') {
-      isSecure = false;
-    }
-    Cookies.set('access_token', tokenData.accessToken, {
-      domain: 'api.narratenet.com',
-      secure: isSecure,
-      expires: new Date(tokenData.expiresAt),
-    });
-    Cookies.set('enc_id_token', tokenData.encryptedIdToken, {
-      domain: 'api.narratenet.com',
-      secure: isSecure,
-      expires: new Date(tokenData.expiresAt),
-    });
-  };
+  // const saveToken = (tokenData) => {
+  //   let isSecure = true;
+  //   if (CONFIGS.ENVIRONMENT.toLowerCase() === 'local') {
+  //     isSecure = false;
+  //   }
+  //   Cookies.set('access_token', tokenData.accessToken, {
+  //     domain: 'api.narratenet.com',
+  //     secure: isSecure,
+  //     expires: new Date(tokenData.expiresAt),
+  //   });
+  //   Cookies.set('enc_id_token', tokenData.encryptedIdToken, {
+  //     domain: 'api.narratenet.com',
+  //     secure: isSecure,
+  //     expires: new Date(tokenData.expiresAt),
+  //   });
+  // };
 
   const resetFirstTimeUserCookie = () => {
     setCookieWithExpiry(STORAGE_KEYS.IS_FIRST_TIME_USER_KEY, false);
